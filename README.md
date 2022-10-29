efe07c11e1235e6459e8ff8c4cf4dd0143480ca4
yossale <yossale@gmail.com>
2022-10-29 12:37:11 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# remindly
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/firebase-gtk-web-start-sgib7c)
---
+++ index.js
@@ -0,0 +1,70 @@
+// Import stylesheets
+import './style.css';
+// Firebase App (the core Firebase SDK) is always required
+import { initializeApp } from 'firebase/app';
+
+// Add the Firebase products and methods that you want to use
+import {
+  signOut,
+  onAuthStateChanged,
+  getAuth,
+  EmailAuthProvider,
+} from 'firebase/auth';
+import {} from 'firebase/firestore';
+
+import * as firebaseui from 'firebaseui';
+
+// Document elements
+const startRsvpButton = document.getElementById('startRsvp');
+const guestbookContainer = document.getElementById('guestbook-container');
+
+const form = document.getElementById('leave-message');
+const input = document.getElementById('message');
+const guestbook = document.getElementById('guestbook');
+const numberAttending = document.getElementById('number-attending');
+const rsvpYes = document.getElementById('rsvp-yes');
+const rsvpNo = document.getElementById('rsvp-no');
+
+let rsvpListener = null;
+let guestbookListener = null;
+
+let db, auth;
+
+async function main() {
+  const firebaseConfig = {
+    apiKey: 'AIzaSyCfS0vpUVbn44mjat5N1sLrrIoRc_hZaG0',
+    authDomain: 'remindly-efe74.firebaseapp.com',
+    projectId: 'remindly-efe74',
+    storageBucket: 'remindly-efe74.appspot.com',
+    messagingSenderId: '1024551308420',
+    appId: '1:1024551308420:web:f8e592aaa91b9db07186d1',
+    measurementId: 'G-BMCFNN93MD',
+  };
+
+  initializeApp(firebaseConfig);
+  auth = getAuth();
+
+  // FirebaseUI config
+  const uiConfig = {
+    credentialHelper: firebaseui.auth.CredentialHelper.NONE,
+    signInOptions: [
+      // Email / Password Provider.
+      EmailAuthProvider.PROVIDER_ID,
+    ],
+    callbacks: {
+      signInSuccessWithAuthResult: function (authResult, redirectUrl) {
+        // Handle sign-in.
+        // Return false to avoid redirect.
+        return false;
+      },
+    },
+  };
+
+  startRsvpButton.addEventListener('click', () => {
+    ui.start('#firebaseui-auth-container', uiConfig);
+  });
+
+  const ui = new firebaseui.auth.AuthUI(auth);
+}
+
+main();
